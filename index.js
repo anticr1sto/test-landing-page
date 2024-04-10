cbc66a67e9adf4460b0f1861edce736afb73999e
vinnie <160987291+anticr1sto@users.noreply.github.com>
2024-04-10 16:04:37 -0300

    up

---
+++ index.js
@@ -0,0 +1,14 @@
+function RolarParaDireita() {
+    Card1.style = "display:none"
+    Card3.style = "display:flex"
+    setadireita.style = "display:none"
+    setaesquerda.style = "display:flex"
+}
+
+function RolarParaEsquerda() {
+    Card1.style = "display:flex"
+    Card3.style = "display:none"
+    setadireita.style = "display:flex"
+    setaesquerda.style = "display:none"
+}
+
